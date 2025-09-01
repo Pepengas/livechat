e8624be5742bf7fb26bb2bec47c2426e48264619
Πέτρος Μανελίδης <96621785+Pepengas@users.noreply.github.com>
2025-09-01 12:43:42 +0300

    Emit full user object for typing events

--- server/services/socket.service.js
+++ server/services/socket.service.js
@@ -20,7 +20,13 @@ const socketService = (io) => {
         // Add user to connected users map
         connectedUsers.set(userData._id, socket.id);
         socket.userId = userData._id;
-        
+        // Preserve basic user info for event payloads
+        socket.user = {
+          _id: userData._id,
+          name: userData.name,
+          avatar: userData.avatar,
+        };
+
         // Update user status in database
         await User.findByIdAndUpdate(userData._id, {
           status: 'online',
@@ -30,7 +36,7 @@ const socketService = (io) => {
 
         // Emit online status to all users
         socket.broadcast.emit('user-online', { userId: userData._id });
-        
+
         // Join a room with the user's ID
         socket.join(userData._id);
         socket.emit('connected');
@@ -81,12 +87,12 @@ const socketService = (io) => {
 
     // Handle typing status
     socket.on('typing', (chatId) => {
-      socket.to(chatId).emit('typing', { chatId, userId: socket.userId });
+      socket.to(chatId).emit('typing', { chatId, user: socket.user });
     });
 
     // Handle stop typing status
     socket.on('stop-typing', (chatId) => {
-      socket.to(chatId).emit('stop-typing', { chatId, userId: socket.userId });
+      socket.to(chatId).emit('stop-typing', { chatId, user: socket.user });
     });
 
     // Handle read messages
