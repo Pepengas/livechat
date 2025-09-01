a94154eea9604ad74514951a4c0ca643ca245419
Πέτρος Μανελίδης <96621785+Pepengas@users.noreply.github.com>
2025-09-01 13:46:32 +0300

    feat: add styled time divider

--- client/src/components/chat/TimeDivider.js
+++ client/src/components/chat/TimeDivider.js
@@ -3,7 +3,9 @@ import { format } from 'date-fns';
 
 const TimeDivider = ({ time }) => (
   <div className="time-divider">
+    <hr />
     <span>{format(new Date(time), 'h:mm a')}</span>
+    <hr />
   </div>
 );
 
